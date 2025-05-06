cd94377f876cc2f4cecf1122672d54f24c5c222e
Kantemir Imamov <imamov@skygate.de>
2025-05-06 12:47:57 +0200

    add db dump

---
+++ packages/sitepackage/ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'sitepackage',
+    'description' => '',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'bootstrap_package' => '15.0.0-15.99.99',
+            'netzbewegung/nb_headless_content_blocks' => ''
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Skygate\\Sitepackage\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Kantemir Imamov',
+    'author_email' => 'imamov@skygate.de',
+    'author_company' => 'Skygate',
+    'version' => '1.0.0',
+];
