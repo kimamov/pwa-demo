a52aeee998754f4ded7179f0f215dd3d547361f4
Kantemir Imamov <imamov@skygate.de>
2025-05-07 11:25:45 +0200

    add formkit for login form

--- front/components/global/T3CeFeloginLogin.vue
+++ front/components/global/T3CeFeloginLogin.vue
@@ -17,8 +17,14 @@ const form=computed(()=>props?.data?.form)
 
 <template>
    <h1>FE LOGIN PLACEHOLDER</h1>
-    <FormKit v-if="form && form.elements?.length" type="form">
-        <FormKit v-for="element of form.elements" :type="element.type" :value="element.value" :name="element.name" :label="element.label"></FormKit>
+    <FormKit
+        :action="`https://pwa-demo.ddev.site/headless${form.action}`"
+        :method="form.method"
+        v-if="form && form.elements?.length"
+        type="form"
+        submit-label="Login"
+    >
+        <FormKit v-for="element of form.elements.filter(el=>el.type!=='submit')" :type="element.type" :value="element.value" :name="element.name" :label="element.label"></FormKit>
     </FormKit>
   <h2>{{uid}}</h2>
     <pre>
