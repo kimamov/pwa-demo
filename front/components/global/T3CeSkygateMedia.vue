13c925b9eb4f912b6b6aae55b13da62f839873a4
Kantemir Imamov <imamov@skygate.de>
2025-07-22 14:44:38 +0200

    add image content block

---
+++ front/components/global/T3CeSkygateMedia.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { T3CeBaseProps } from '@t3headless/nuxt-typo3';
+
+interface T3CeImageWithDescription extends T3CeBaseProps {
+    header?: string
+    image?: string
+}
+const props = withDefaults(defineProps<T3CeImageWithDescription>(), {
+})
+</script>
+
+<template>
+    <h2>{{uid}}</h2>
+  <h2>{{header}}</h2>
+  <img v-if="image" v-for="data of image" :src="data.publicUrl" alt="">
+</template>
+
