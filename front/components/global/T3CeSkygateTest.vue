799bf50ebf80d2f3e57772ad781740cbe679d50d
Kantemir Imamov <imamov@skygate.de>
2025-07-22 13:39:22 +0200

    update docu for typo3 content blocks

--- front/components/global/T3CeSkygateTest.vue
+++ front/components/global/T3CeSkygateTest.vue
@@ -2,24 +2,18 @@
 import type { T3CeBaseProps } from '@t3headless/nuxt-typo3';
 
 interface T3CeImageWithDescription extends T3CeBaseProps {
-    // bodytext: string,
-    // assets?: T3File[]
     header?: string
     title?: string
 }
 const props = withDefaults(defineProps<T3CeImageWithDescription>(), {
-    // bodytext: ''
+  header: 'header fallback',
+  title: 'title fallback'
 })
-console.log({props})
 </script>
 
 <template>
-    <h1>skygate test</h1>
     <h2>{{uid}}</h2>
     <h2>{{header}}</h2>
     <h3>{{title}}</h3>
 </template>
 
-<style scoped>
-
-</style>
