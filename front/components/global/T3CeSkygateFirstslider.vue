af7f7a4477060d305afbc02fe7fe26a4aa18a4d5
Amir Jelodar <jelodar@skygate.de>
2025-07-31 15:31:09 +0200

    installed Swiper-Slider and update slider component

--- front/components/global/T3CeSkygateFirstslider.vue
+++ front/components/global/T3CeSkygateFirstslider.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import type { T3CeBaseProps } from '@t3headless/nuxt-typo3';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
+// import './style.css';
 
 interface T3CeImageWithDescription extends T3CeBaseProps {
   header?: string;
@@ -17,6 +23,9 @@ interface T3CeImageWithDescription extends T3CeBaseProps {
     slide_description: string;
   }[];
 }
+const cleanHtml = (html: string) => {
+  return html.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+};
 
 const props = withDefaults(defineProps<T3CeImageWithDescription>(), {
   slider: []
@@ -33,28 +42,72 @@ const slides = computed(() => {
   }));
 });
 
-const cleanHtml = (html: string) => {
-  return html.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-};
-
+const modules = ref([Navigation, Pagination]);
 
 </script>
 
 <template>
   <h2>{{ header }}</h2>
-  <div v-if="slides" class="slider-overview">
-    <div v-for="(slide, index) in slides" :key="index" class="slide-card">
-      <div v-if="slide.slideImage" class="slide-image">
-        <img :src="slide.slideImage" :alt="slide.slideTitle" />
+  <section class="section">
+    <div class="container">
+      <div class="slider-container">
+        <Swiper
+            :cssMode="true"
+            :slidesPerView="1"
+            :spaceBetween="0"
+            :loop="false"
+            :keyboard="{
+              enabled: true,
+            }"
+            :navigation="true"
+            :pagination="{
+              clickable: true,
+            }"
+            :breakpoints=" {
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+              },
+              1440: {
+                slidesPerView: 3,
+                spaceBetween: 30
+              }
+            }"
+            :modules="modules"
+            class="mySwiper"
+        >
+          <SwiperSlide class="amir" v-for="(slide, index) in slides" :key="index">
+            <div class="slide-card">
+              <h3>{{ slide.slideTitle }}</h3>
+              <p v-html="slide.slideDescription"></p>
+              <a :href="slide.slideLink">Mehr erfahren</a>
+              <img :src="slide.slideImage" :alt="slide.slideTitle" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
-      <h3 class="slide-title">{{ slide.slideTitle }}</h3>
-      <div class="slide-description" v-html="cleanHtml(slide.slideDescription)"></div>
-      <a :href="slide.slideLink" class="slide-link">Mehr erfahren</a>
     </div>
-  </div>
+  </section>
+  <section class="section">
+    <div class="container">
+      <div v-if="slides" class="slider-overview">
+        <div v-for="(slide, index) in slides" :key="index" class="slide-card">
+          <div v-if="slide.slideImage" class="slide-image">
+            <img :src="slide.slideImage" :alt="slide.slideTitle" />
+          </div>
+          <h3 class="slide-title">{{ slide.slideTitle }}</h3>
+          <div class="slide-description" v-html="cleanHtml(slide.slideDescription)"></div>
+          <a :href="slide.slideLink" class="slide-link">Mehr erfahren</a>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
+.section {
+  padding: 32px 0;
+}
 .slider-overview {
   display: flex;
   flex-wrap: wrap;
@@ -72,6 +125,7 @@ const cleanHtml = (html: string) => {
   text-align: center;
   background-color: #fff;
   transition: transform 0.3s ease;
+  margin-bottom: auto;
 
   &:hover {
     transform: scale(1.05);
@@ -110,5 +164,45 @@ const cleanHtml = (html: string) => {
       border-radius: 8px;
     }
   }
+}
+
+.headline {
+  color: inherit;
+  line-height: 150%;
+  margin-bottom: 1.125rem;
+  margin-top: 0;
+  font-style: normal;
+  font-stretch: normal;
+  font-optical-sizing: auto;
+  font-weight: normal;
+
+  &--white {
+    color: #ffffff;
+  }
+
+  &--h1 {
+    font-size: 3rem;
+  }
+  &--h2 {
+    font-size: 2.5rem;
+  }
+  &--h3 {
+    font-size: 2rem;
+  }
+  &--h4 {
+    font-size: 1.25rem;
+  }
+}
+h1 {
+  font-size: 3rem;
+}
+h2 {
+  font-size: 2.5rem;
+}
+h3 {
+  font-size: 2rem;
+}
+h4 {
+  font-size: 1.25rem;
 }
 </style>
